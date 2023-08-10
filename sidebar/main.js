20a5cbb86b87551287bcdc90c93e0bbaf4a6d12c
Rifki ahmad fahrezi <56358927+rifkiahmadfahrezi@users.noreply.github.com>
2023-08-10 14:00:30 +0700

    Update main.js
    add javascript for show and hiding sidebar

--- sidebar/main.js
+++ sidebar/main.js
@@ -1,1 +1,17 @@
+const sidebar = document.querySelector(".sidebar")
+const menuToggler = document.querySelector(".menu-toggler")
+const logo = document.querySelector("nav .logo")
 
+
+menuToggler.addEventListener("click", () => {
+  if (!sidebar.classList.contains("show")){
+    logo.style.color = "white"
+    sidebar.classList.add("show")
+    sidebar.style.left = "0"
+  }else{
+    sidebar.classList.remove("show")
+     sidebar.style.left = "-250px"
+     logo.style.color = "black"
+  }
+  
+})
