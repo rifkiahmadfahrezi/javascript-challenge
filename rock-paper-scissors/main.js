2b4def2646b0ee0c9c8c90c739e4ca695ab0de87
Rifki ahmad fahrezi <56358927+rifkiahmadfahrezi@users.noreply.github.com>
2023-09-01 20:14:51 +0700

    add get opponent function

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,8 +1,10 @@
 const selectImages = document.querySelectorAll("[data-character]")
 const charWrapper = document.querySelector(".char-wrapper")
+const btnWrapper = document.querySelector(".btn-wrapper")
 
 selectImages.forEach((img,i) => {
-  let character = img.dataset.character
+  const character = img.dataset.character
+  const opponent = getOpponent()
   img.addEventListener("click", e => {
     document.querySelector(".title").innerText = `Your character is : ${character}`
     
@@ -12,9 +14,9 @@ selectImages.forEach((img,i) => {
         el.classList.add("remove")
         setTimeout(() => {
           el.style.display = "none"
-          
-          charWrapper.innerHTML += `<a href="#" onclick="reSelectChar()" class="cancel-btn">cancel</a>`
-          
+          btnWrapper.innerHTML = `
+          <a href="#" onclick="resetChar()" data-action="cancel" class="action-btn">cancel</a>
+          <a href="#" onclick="fight(${character},${opponent})" data-action="fight" class="action-btn">fight!</a>`
         },500)
       }
     })
@@ -23,12 +25,43 @@ selectImages.forEach((img,i) => {
 
 function getOpponent(){
   const characters = ["rock","paper","scissors"]
-  return character[Math.floor(Math.random() * characters.length)]
+  return characters[Math.floor(Math.random() * characters.length)]
 }
-function reSelectChar(el){
+function resetChar(){
   charWrapper.innerHTML = ""
+  document.querySelector(".title").innerText = "Select your character"
   selectImages.forEach((el,i) => {
-    charWrapper.appendChild(selectImages[i])
-    selectImages[i].classList.remove("remove")
+    el.style.display = "inline"
+    el.classList.remove("remove")
+    el.classList.remove("selected")
+    charWrapper.appendChild(el)
+    btnWrapper.innerHTML = ""
   })
+}
+function fight(myChar,opponent){
+  charWrapper.innerHTML += `VS
+  <img src="${opponent}.jpg" data-character="${opponent}" alt="${opponent} image">
+  `
+  const title = document.querySelector(".title")
+  if (myChar == opponent){
+    title.innerText = "DRAW"
+  }else if(myChar == "scissors"){
+    if (opponent == "rock"){
+      title.innerText = "YOU LOSE!" 
+    }else{
+      title.innerText = "YOU WIN!" 
+    }
+  }else if(myChar == "rock"){
+    if (opponent == "paper"){
+      title.innerText = "YOU LOSE!" 
+    }else{
+      title.innerText = "YOU WIN!" 
+    }
+  }else if(myChar == "paper"){
+    if (opponent == "scissors"){
+      title.innerText = "YOU LOSE!" 
+    }else{
+      title.innerText = "YOU WIN!" 
+    }
+  }
 }
